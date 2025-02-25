8fcac619349a5231a693299261153a15ae0b6aa0
Chao Zhang <zhangchao@microsoft.com>
2025-02-25 17:08:29 +0800

    run model

--- examples/bert/run_qdq_model.py
+++ examples/bert/run_qdq_model.py
@@ -3,6 +3,7 @@
 import onnxruntime
 import numpy as np
 from transformers import BertTokenizer
+import torch
 
 options = onnxruntime.SessionOptions()
 options.add_session_config_entry("session.disable_cpu_ep_fallback", "1")
@@ -24,10 +25,16 @@ encoded_input = tokenizer(
 
 result = session.run(["logits"],
                      {
-                        "input_ids": encoded_input['input_ids'],
-                        "attention_mask": encoded_input['attention_mask'],
-                        "token_type_ids": encoded_input['token_type_ids']
+                        "input_ids": np.array(encoded_input['input_ids']).astype(np.int64),
+                        "attention_mask": np.array(encoded_input['attention_mask']).astype(np.int64),
+                        "token_type_ids": np.array(encoded_input['token_type_ids']).astype(np.int64)
                     })
 
-# Print output.
-print(result)
+outputs = result[0]
+masked_index = torch.nonzero(encoded_input['input_ids'].squeeze() == tokenizer.mask_token_id, as_tuple=False).squeeze(-1)
+
+logits = outputs[0, masked_index, :]
+probs = torch.from_numpy(logits).softmax(dim=-1)
+values, predictions = probs.topk(5)
+for v, p in zip(values, predictions):
+   print(f"score: {v}, token: {p}, token_str: {tokenizer.decode([p])}")
