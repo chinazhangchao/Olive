ae9c427894beff7a98850e3aa26f2ddb826f9f40
Chao Zhang <zhangchao@microsoft.com>
2025-02-28 14:39:19 +0800

    get probs complete

--- examples/bert/mlm_user_script.py
+++ examples/bert/mlm_user_script.py
@@ -3,11 +3,13 @@
 # Licensed under the MIT License.
 # --------------------------------------------------------------------------
 import numpy as np
+import random
 import torch
 from datasets import load_dataset
 from torch.utils.data import Dataset
 from transformers import BertTokenizer
 from olive.data.registry import Registry
+import evaluate
 
 class QuantizationDataset(Dataset):
     def __init__(self, model_name, start=0, end=100):
@@ -42,49 +44,45 @@ class QuantizationDataset(Dataset):
 def quantization_dataset(**kwargs):
     return QuantizationDataset(**kwargs)
 
-class MLMDataset(Dataset):
-    def __init__(
-        self,
-        model_name,
-        start=0,
-        end=100
-    ):
-        assert 0 <= start < end
-        self.start = start
-        self.end = end
-        self.model_name = model_name
-        self.processor = CLIPProcessor.from_pretrained(self.model_name)  # max_length = 77 for input_ids
-        self.length = self.end - self.start
-
-        dataset = load_dataset(self.dataset_name, split=f"test[{self.start}:{self.end}]")
-        text_inputs = self.processor(
-            text=[" ".join(item["caption"]) for item in dataset],
-            return_tensors="np",
-            padding="max_length",
-            truncation=True,
-        )
-
-        self.model_inputs = [
-            {
-                "input_ids": text_inputs["input_ids"].astype(np.int64),
-                "attention_mask": text_inputs["attention_mask"].astype(np.int64),
-            }
-        ]
-
-    def __len__(self):
-        return self.length
-
+class MLMDataset(QuantizationDataset):
     def __getitem__(self, idx):
-        return self.model_inputs[idx], torch.Tensor([idx]).to(torch.int32)
+        item = next(self.it)
+        # Replace random word with [MASK]
+        words = item["text"].split()
+        random_idx = random.randint(0, min(30, len(words)))
+        words[random_idx] = "[MASK]"
+        masked_text = " ".join(words)
 
+        encoded_input = self.tokenizer(
+                    masked_text,
+                    padding="max_length",
+                    max_length= 128,
+                    truncation=True,
+                    return_tensors='np')
+
+        return {
+                "input_ids": encoded_input['input_ids'].astype(np.int64),
+                "attention_mask": encoded_input['attention_mask'].astype(np.int64),
+                "token_type_ids": encoded_input['token_type_ids'].astype(np.int64)
+            }, torch.Tensor([random_idx]).to(torch.int32)
 
 @Registry.register_dataset()
 def mlm_dataset(**kwargs):
     return MLMDataset(**kwargs)
 
-@Registry.register_post_process()
-def clip_post_process(output):
-    return output["logits_per_image"].argmax(axis=-1)
+def calc_perplexity(model_output, targets):
+    # model_output[0]: preds, model_output[1]: logits
+    # calculate metric
+    # return metric value
 
-def eval_perplexity(model, device, execution_providers):
-    pass
+    probs = model_output.logits[torch.arange(model_output.logits.size(dim=0)),targets,:].softmax(dim=-1)
+    _, predictions = probs.topk(5)
+    for p in predictions:
+        tokenizer.decode([p])
+
+    perplexity = evaluate.load("perplexity", module_type="measurement")
+    input_texts = ["Hello I'm a model model.", "Hello I'm a world model.", "Hello I'm a data model.", "Hello I'm a flight model.", "Hello I'm a business model." ]
+    results = perplexity.compute(model_id='gpt2',
+                                add_start_token=False,
+                                data=input_texts)
+    results["mean_perplexity"]
