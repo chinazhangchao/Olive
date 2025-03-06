43895fa29f5da975b12542c61dcdf606fa4864f2
Chao Zhang (from Dev Box) <zhangchao@microsoft.com>
2025-03-06 14:24:50 +0800

    run clip model succeed

--- examples/clip/run_model.py
+++ examples/clip/run_model.py
@@ -4,24 +4,27 @@ import requests
 from transformers import CLIPProcessor
 import onnxruntime
 import numpy as np
+import torch
 
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
-inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="np", padding=True)
+inputs = processor(text=["a photo of a cat", "a photo of a dog", "a photo of a cat", "a photo of a dog", "a photo of a cat", "a photo of a dog", "a photo of a cat", "a photo of a dog", "a photo of a cat", "a photo of a dog"],
+                images=image, return_tensors="np", padding="max_length",
+                max_length= 77, truncation=True)
 
 options = onnxruntime.SessionOptions()
 session = onnxruntime.InferenceSession(r"C:\Users\zhangchao\project\Olive\examples\clip\models\clip-vit-base-patch32\model.onnx",
                                        providers=["QNNExecutionProvider"],
                                        provider_options=[{"backend_path": "QnnHtp.dll"}])
-outputs = session.run(["logits"],
+logits_per_image = session.run(["logits_per_image"],
                      {
                         "input_ids": inputs['input_ids'].astype(np.int64),
                         "attention_mask": inputs['attention_mask'].astype(np.int64),
-                        "pixel_values": inputs['pixel_values'].astype(np.int64)
+                        "pixel_values": inputs['pixel_values']
                     })
-logits_per_image = outputs.logits_per_image
-probs = logits_per_image.softmax(dim=1)
+
+probs = torch.tensor(logits_per_image[0]).softmax(dim=1)
 print("Label probs:", probs)
